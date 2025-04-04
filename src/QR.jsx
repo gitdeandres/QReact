4cb7857dc039b2f313d9c854f53d9f1228a19b09
Andrés <andres.ravelo@bioquimica.cl>
2025-04-04 11:54:59 -0300

    se traspasan los estilos del QR al HTML como clases tailwind

--- src/QR.jsx
+++ src/QR.jsx
@@ -3,7 +3,7 @@ import { QRCodeCanvas } from 'qrcode.react';
 export function QRCode ({ url, ref }) {
   return (
     <section className='h-100 w-[80%] place-items-center place-content-center !mx-auto'>
-      <span id='qrcode' className='block relative' ref={ref}>
+      <span id='qrcode' className='block relative border-40 border-solid border-white rounded-3xl' ref={ref}>
         <QRCodeCanvas
           value={url}
           title='Código QR'
