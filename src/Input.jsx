947f2e3914f0ed9808602af12d390c192439a591
Andrés <andres.ravelo@bioquimica.cl>
2025-04-04 11:56:30 -0300

    se incluye clase tailwind para modificar el color de la fuente del placeholder

--- src/Input.jsx
+++ src/Input.jsx
@@ -5,7 +5,7 @@ export function Input ({ value, onChange }) {
       type='url'
       name='url'
       placeholder='Enter an url'
-      className='w-full h-15 border-2 border-solid border-(--blue) focus-visible:outline-(--white) focus-visible:outline-solid rounded-2xl !p-8 !pr-[28%] bg-(--dark)'
+      className='w-full h-15 border-2 border-solid border-(--blue) focus-visible:outline-(--white) focus-visible:outline-solid rounded-2xl !p-8 !pr-[28%] bg-(--dark) placeholder:text-current'
       value={ value }
       onChange={ onChange }
       autoComplete='off'
