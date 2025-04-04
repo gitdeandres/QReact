bd2e4ff1a36df8e09fc809b70d1a7f4e9a5cedc7
Andrés <andres.ravelo@bioquimica.cl>
2025-04-04 11:57:20 -0300

    se convierte en flex el elemento que contiene el QR

--- src/QR.jsx
+++ src/QR.jsx
@@ -2,7 +2,7 @@ import { QRCodeCanvas } from 'qrcode.react';
 
 export function QRCode ({ url, ref }) {
   return (
-    <section className='h-100 w-[80%] place-items-center place-content-center !mx-auto'>
+    <section className='flex h-100 w-[80%] place-items-center place-content-center !mx-auto'>
       <span id='qrcode' className='block relative border-40 border-solid border-white rounded-3xl' ref={ref}>
         <QRCodeCanvas
           value={url}
